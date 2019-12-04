61d132b3fb09d0611a91e7ab6774bd6502253050
Arun Shenoy <devarunshenoy99@gmail.com>
2019-12-04 11:48:37 +0530

    Final commit

--- entry.py
+++ entry.py
@@ -14,6 +14,7 @@ sql2 = "INSERT INTO FAT(FID,TOTAL_FAT,SATURATED_FAT) VALUES (%s,%s,%s)"
 sql3 = "INSERT INTO VITAMIN(FID,A,A_RAE,B12,B6,C,D,E,K) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 sql4 = "INSERT INTO PROTEIN(FID,PROTEIN) VALUES (%s,%s)"
 sql5 = "INSERT INTO MINERAL(FID,SODIUM,CALCIUM,IRON,POTASSIUM,CARB,WATER) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+#CREATE AN ENTRY FOR DEFICIENCIES TOO
 val1 = [tuple([int(dat[0])+1,dat[1],dat[2][:-1],dat[3]])for dat in data[1:]]
 val2 = [tuple([int(dat[0])+1,dat[4][:-1],dat[5][:-1]]) for dat in data[1:]]
 val3 = [tuple([int(dat[0])+1,dat[15][:-2],dat[16][:-3],dat[22][:-3],dat[23][:-2],dat[24][:-2],dat[25][:-2],dat[26][:-2],dat[28][:-3]]) for dat in data[1:]]
